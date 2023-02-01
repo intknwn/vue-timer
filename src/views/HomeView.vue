ec294a80f2fbab66a0f2d83cc43480a29aa37094
Dmitriy Nekliudov <intknwn@gmail.com>
2023-02-01 11:58:54 +0300

    initial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <Main />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import Main from '@/components/MainScreen.vue'
 
-export default {
-  name: "HomeView",
+export default defineComponent({
+  name: 'HomeView',
+
   components: {
-    HelloWorld,
+    Main,
   },
-};
+})
 </script>
